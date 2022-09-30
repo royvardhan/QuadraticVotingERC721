42e0b60cbbfcf70c5368fd100975b9632bbb6176
royvardhan <royvardhan.official@gmail.com>
2022-09-30 21:26:30 +0530

    .

--- frontend/voting-dapp/pages/components/Header.js
+++ frontend/voting-dapp/pages/components/Header.js
@@ -9,11 +9,12 @@ export default function Header() {
   //   //   }
 
   return (
-    <div>
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 p-3 flex justify-end">
+      <h4 className="">Quadratic Voting NFTS</h4>
       <ConnectButton
         accountStatus={{
           smallScreen: "avatar",
-          largeScreen: "full",
+          largeScreen: "small",
         }}
       />
     </div>
