56e6029783e5a3d86188b18fdfb52bed5a8478c9
royvardhan <royvardhan.official@gmail.com>
2022-10-01 22:02:34 +0530

    contract deployed

--- frontend/voting-dapp/pages/components/ProposalCards.js
+++ frontend/voting-dapp/pages/components/ProposalCards.js
@@ -2,17 +2,38 @@ import React, { useState } from "react";
 
 export default function ProposalCards() {
   return (
-    <div className="flex justify-center">
-      <div className="mt-10 p-5 backdrop-opacity-20 backdrop-invert bg-white/50 ">
-        <p className="text-lg">
-          NFT Address: 0xdEE878E02c069c4c5AD37299f51E9C3Cd6c8CD4b
-        </p>
-        <p>Proposal ID: 578</p>
-        <p className="">
-          Description: Use funds from Community Wallet to buy BAYC NFTs from
-          Opensea and hold them till 2030
-        </p>
+    <div>
+      <div className="flex justify-center ">
+        <div className="mt-10 w-auto p-5 backdrop-opacity-20 backdrop-invert bg-white/50 max-w-xl rounded-3xl">
+          <p className=" font-semibold">
+            NFT Address: 0xdEE878E02c069c4c5AD37299f51E9C3Cd6c8CD4b
+          </p>
+          <p>Proposal ID: 578</p>
+          <p className="">
+            Description: Use funds from Community Wallet to buy BAYC NFTs from
+            Opensea and hold them till 2030.
+          </p>
+          <p>Voters: 76</p>
+          <p>Status: In-Progress</p>
+        </div>
+      </div>
+
+      <div className="flex justify-center ">
+        <div className="mt-10 w-auto p-5 backdrop-opacity-20 backdrop-invert bg-white/50 max-w-xl rounded-3xl">
+          <p className=" font-semibold">
+            NFT Address: 0xdEE878E02c069c4c5AD37299f51E9C3Cd6c8CD4b
+          </p>
+          <p>Proposal ID: 578</p>
+          <p className="">
+            Description: Use funds from Community Wallet to buy BAYC NFTs from
+            Opensea and hold them till 2030.
+          </p>
+          <p>Voters: 76</p>
+          <p>Status: In-Progress</p>
+        </div>
       </div>
     </div>
   );
 }
+
+// for decoding bytes into strings  = ethers.utils.defaultAbiCoder.decode(["string"], cid)
---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const { ethers, run, network } = require("hardhat");
+
+async function main() {
+  const contractFactory = await ethers.getContractFactory(
+    "QuadraticVotingERC721"
+  );
+  console.log("------------Deploying Contract-----------");
+  const contractVoting = await contractFactory.deploy();
+  await contractVoting.deployed();
+  console.log("Contract deployed at: " + (await contractVoting.address));
+  await verify(contractVoting.address, []);
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
